44a9f5bb7830fadb74f54a5d0b8e46ed7926daef
Ilyes <ilyes.smaoui94@gmail.com>
2025-08-13 04:23:37 +0200

    Started day 7

---
+++ day_07/day_07.py
@@ -0,0 +1,12 @@
+# open file, collect content
+le_filename = "day_07_input.txt"
+le_file = open(le_filename, "r")
+le_file_content = le_file.read()
+
+######
+# PART 1
+######
+
+######
+# PART 2
+######
