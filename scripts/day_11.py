33f580505d566d48bb5011d476a01bd8cd0423ac
Ilyes <ilyes.smaoui94@gmail.com>
2025-08-18 07:11:51 +0200

    Started working on Day 11

---
+++ scripts/day_11.py
@@ -0,0 +1,14 @@
+import helpers
+
+# open file, collect content
+le_filename = "../inputs/day_XX_input.txt"
+le_file_content = helpers.get_file_content_raw(le_filename)
+le_stones = [int(stone_str) for stone_str in le_file_content.split(" ")]
+
+######
+# PART 1
+######
+
+######
+# PART 2
+######
