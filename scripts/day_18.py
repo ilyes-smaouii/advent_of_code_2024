6b360b4ebdc5069250af141e3339c7b847e17d85
Ilyes <ilyes.smaoui94@gmail.com>
2025-08-25 02:44:31 +0200

    Just started Day 18

---
+++ scripts/day_18.py
@@ -0,0 +1,21 @@
+import helpers
+
+# open file, collect content
+le_filename = "../inputs/day_18_input.txt"
+# le_file_content = helpers.get_file_content_raw(le_filename)
+le_lines = helpers.get_file_content_as_lines(le_filename)
+# le_char_table = helpers.get_file_content_as_table(le_filename)
+
+######
+# PART 1
+######
+
+# 1 - Parse lines
+# 2 - add walls
+# 3 - A-star on resulting maze
+# 4 - ?
+# 5 - Profit
+
+######
+# PART 2
+######
