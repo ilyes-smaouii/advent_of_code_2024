bc9ed1847abed4dcf27854d40dd45e3c7784b979
Ilyes <ilyes.smaoui94@gmail.com>
2025-08-26 03:40:40 +0200

    Started Day 19

---
+++ scripts/day_19.py
@@ -0,0 +1,15 @@
+import helpers
+
+# open file, collect content
+le_filename = "../inputs/day_19_input.txt"
+le_file_content = helpers.get_file_content_raw(le_filename)
+# le_lines = helpers.get_file_content_as_lines(le_filename)
+# le_char_table = helpers.get_file_content_as_table(le_filename)
+
+######
+# PART 1
+######
+
+######
+# PART 2
+######
